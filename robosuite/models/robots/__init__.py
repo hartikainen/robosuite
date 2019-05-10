47b7fe6d24d4f5c35e9b27a81f1794f2bfefccd1
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-05-09 18:46:08 -0700

    Add InvisibleArm robot

--- robosuite/models/robots/__init__.py
+++ robosuite/models/robots/__init__.py
@@ -1,3 +1,4 @@
 from .robot import Robot
 from .sawyer_robot import Sawyer
 from .baxter_robot import Baxter
+from .invisible_arm_robot import InvisibleArm
---
+++ robosuite/models/robots/invisible_arm_robot.py
@@ -0,0 +1,29 @@
+import numpy as np
+from robosuite.models.robots.robot import Robot
+from robosuite.utils.mjcf_utils import xml_path_completion, array_to_string
+
+
+class InvisibleArm(Robot):
+    """InvisibleArm is a witty invisible-arm robot designed by no one."""
+
+    def __init__(self):
+        super().__init__(xml_path_completion("robots/invisible_arm/robot.xml"))
+
+        self.bottom_offset = np.array([0, 0, 0])
+
+    def set_base_xpos(self, pos):
+        """Places the robot on position @pos."""
+        node = self.worldbody.find("./body[@name='base']")
+        node.set("pos", array_to_string(pos - self.bottom_offset))
+
+    @property
+    def dof(self):
+        return 3
+
+    @property
+    def joints(self):
+        return ("arm_x_joint", "arm_y_joint", "arm_z_joint")
+
+    @property
+    def init_qpos(self):
+        return np.array((0.0, 0.0, 0.0))
